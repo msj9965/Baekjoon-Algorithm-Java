598aa4570e34e826fc5caec47464bd7a52bed642
msj9965 <121001545+msj9965@users.noreply.github.com>
2022-12-24 19:27:13 +0900

    Add files via upload

---
+++ 단계별로 풀어보기/사칙연산/First.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class First {
+    public static void main(String[]ar){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+
+
+
+    }
+}
